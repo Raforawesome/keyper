4c5bfda11b08cb6dc3b32ecb61a80a3108fa39b5
Raforawesome <ubuntuhasnoroblox@gmail.com>
2022-04-30 00:16:15 -0400

    Made Perky.write_values() functional (will drop request if one is currently active, no queue system set up yet)

--- src/perky.rs
+++ src/perky.rs
@@ -9,6 +9,7 @@ pub struct Perky {
 	file_name: String,
 	data: Dictionary,
 	mutex: bool,
+	file: Option<fs::File>,
 	// queue: Vec<u8>,
 	// queue_handle: Option<JoinHandle<()>>
 }
@@ -22,18 +23,22 @@ impl Default for Perky {
 // Associated functions
 impl Perky {
 	pub fn new<T: ToString>(auto_write: bool, file_name: T) -> Perky {
+		let mut t = std::env::current_exe().unwrap().parent().unwrap().to_path_buf();
+		t.push(PathBuf::from(file_name.to_string()));
 		let data: Dictionary = Dictionary::new();
-		if auto_write {
-			let res = fs::File::create(PathBuf::from(file_name.to_string()));
-			if let Err(e) = res {
-				println!("WARNING: Error in creating file for new Keyper instance :: {:?}", e);
-			}
+		let res = fs::File::create(t);
+		let mut writable: Option<fs::File> = None;
+		if let Err(e) = res {
+			println!("WARNING: Error in creating file for new Keyper instance :: {:?}", e);
+		} else if let Ok(f) = res {
+			writable = Some(f);
 		}
 		Perky {
 			auto_write,
 			file_name: file_name.to_string(),
 			data,
 			mutex: false,
+			file: writable
 			// queue: Vec::new(),
 			// queue_handle: None
 		}
@@ -66,8 +71,25 @@ impl Perky {
 		// }
 		res
 	}
-	// fn write_values(&mut self) {
-	// 	self.mutex = true;
-	// 	self.mutex = false;
-	// }
+	fn write_values(&mut self) {
+		if !self.mutex {
+			if let Some(f) = &self.file {
+				self.mutex = true;
+				let file_name = &self.file_name;
+				let mut t = std::env::current_exe().unwrap().parent().unwrap().to_path_buf();
+				t.push(PathBuf::from(file_name.to_string()));
+				let mut file: String = String::new();
+				for i in 0..self.data.keys.len() {
+					let k = &self.data.keys[i];
+					let v = &self.data.values[i];
+					file = file + k.as_str() + "|PERKY_SEP|" + v.as_str() + "\n";
+				}
+				let res = fs::write(t, file);
+				if res.is_err() {
+					println!("Perky :: WARNING: Writing to file failed");
+				}
+				self.mutex = false;
+			}
+		}
+	}
 }
